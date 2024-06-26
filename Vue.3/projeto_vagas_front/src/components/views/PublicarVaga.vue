8d881fbe8f97b2a94ac711a1ca04713b5a7d7e6f
Clayton <_clayton@outlook.com>
2024-06-26 10:55:40 -0300

    Componente PublicarVaga
    criando os campos para cadastro das vagas

--- Vue.3/projeto_vagas_front/src/components/views/PublicarVaga.vue
+++ Vue.3/projeto_vagas_front/src/components/views/PublicarVaga.vue
@@ -1,12 +1,67 @@
 <template>
-    <div>
-        <h3>Publicar Vagas</h3>
+    <div class="container py-4">
+        <div class="row">
+            <div class="col">
+                <h3>Publicar Vagas</h3>
+            </div>
+        </div>
+        <!-- cadastrar vaga -->
+        <div class="row mt-2">
+            <div class="col">
+                <label class="form-label">Título da vaga</label>
+                <input type="text" class="form-control">
+                <div class="form-text">Exemplo: Dev Javascript</div>
+            </div>
+        </div>
+
+        <div class="row mt-2">
+            <div class="col">
+                <label class="form-label">Descrição</label>
+                <textarea type="text" class="form-control"></textarea>
+                <div class="form-text">Informe os detalhes da vaga</div>
+            </div>
+        </div>
+
+        <div class="row mt-2">
+            <div class="col">
+                <label class="form-label">Salário</label>
+                <input type="number" class="form-control">
+                <div class="form-text">Informe o salário</div>
+            </div>
+
+            <div class="col">
+                <label class="form-label">Modalidade</label>
+                <select class="form-select">
+                    <option value="1">Home office</option>
+                    <option value="2">Presencial</option>
+                </select>
+                <div class="form-text">Informe onde as atividades serão realizadas</div>
+            </div>
+
+            <div class="col">
+                <label class="form-label">Tipo</label>
+                <select class="form-select">
+                    <option value="1">CLT</option>
+                    <option value="2">PJ</option>
+                </select>
+                <div class="form-text">Informe o tipo de contratação</div>
+            </div>
+        </div>
+
+        <!-- enviar vaga -->
+        <div class="row mt-3">
+            <div class="col">
+                <button type="submit" class="btn btn-primary">
+                    Cadastrar
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     export default{
-    name: 'Vagas'
+    name: 'PublicarVagas'
  }
 </script>
 
